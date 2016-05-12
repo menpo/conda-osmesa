377efb0f9c474b7ef95e0d1bdb535e1eb0d76d30
Patrick Snape <patricksnape@gmail.com>
2016-05-12 09:43:24 +0200

    Initial commit
    should build osmesa with gallium/llvmpipe on Linux and
    OSX.

---
+++ conda/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# LLVMdev contains std::move calls which are not supported by the 10.6/10.7
+# libc++. So use 10.9
+export MACOSX_DEPLOYMENT_TARGET=10.9
+
+autoreconf -fi
+
+./configure                                             \
+	CXXFLAGS="-O2 -g -DDEFAULT_SOFTWARE_DEPTH_BITS=31"  \
+	CFLAGS="-O2 -g -DDEFAULT_SOFTWARE_DEPTH_BITS=31"    \
+	--enable-texture-float                              \
+	--disable-xvmc                                      \
+	--disable-glx                                       \
+	--disable-dri                                       \
+	--with-dri-drivers=""                               \
+	--with-gallium-drivers="swrast"                     \
+	--disable-shared-glapi                              \
+	--disable-egl                                       \
+	--with-egl-platforms=""                             \
+	--enable-gallium-osmesa                             \
+	--enable-gallium-llvm=yes                           \
+	--disable-gles1                                     \
+	--disable-gles2                                     \
+	--prefix=$PREFIX                                    \
+	--disable-llvm-shared-libs
+
+make -j$CPU_COUNT
+make install
